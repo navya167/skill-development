c55f0f6db7bcbbf183d61473c08ca23394e39a58
Navya <Navya>
2021-07-08 06:43:43 +0530

    added CORS module

--- app/serviceapp.py
+++ app/serviceapp.py
@@ -1,19 +1,26 @@
 import re
 from flask import Flask,render_template,request
+from flask_cors import CORS,cross_origin
 import sys
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
+
 
 @app.route('/')
+@cross_origin(supports_credentials=True)
 def loadform():
     print(sys.version)
     return render_template('index.html')
 
 
 @app.route('/result',methods = ['POST','GET'])
+@cross_origin(supports_credentials=True)
 def result():
    data =  request.args.get('data')
-   import calculator
-   result = calculator.calculate(data)
+#    import calculator
+   from app.calculator import calculate
+   result = calculate(data)
    print('result',result,type(result))
    if type(result) == str:
        return {'result':result},400
